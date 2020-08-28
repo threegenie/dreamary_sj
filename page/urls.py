9831f137a325da32faa9bb73937a47ec29f1139a
threegenie <63702924+threegenie@users.noreply.github.com>
2020-08-28 16:47:19 +0900

    add dreamary crud

--- page/urls.py
+++ page/urls.py
@@ -5,5 +5,9 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('introduce/',views.introduce, name='introduce'),
     path('profile/<int:designer_id>', views.detail, name = "detail"),
+    path('new/', views.new, name = "new"),
+    path('create/', views.create, name="create"),
+    path('delete/<int:designer_id>/', views.delete, name="delete"),
+    path('update/<int:designer_id>/', views.update, name="update"),
 
 ]
--- page/views.py
+++ page/views.py
@@ -13,4 +13,42 @@ def detail(request, designer_id):
     designer = get_object_or_404(Designer, pk=designer_id)
     return render(request, "detail.html", {'designer' : designer})
 
+def new(request):
+    return render(request,"new.html")
+
+def create(request):
+    if request.method == 'POST':
+        post = Designer()
+        if 'image' in request.FILES:
+            post.Image = request.FILES['image']
+        post.name = request.POST['name']
+        post.address = request.POST['address']
+        post.description = request.POST['description']
+
+        post.save()
+
+        return redirect('detail', post.id)
+
+
+def delete(request, designer_id):
+    post = get_object_or_404(Designer, pk=designer_id)
+    post.delete()
+
+    return redirect('home')
+
+
+def update(request, designer_id):
+    post = get_object_or_404(Designer, pk=designer_id)
+
+    if request.method == 'POST':
+        if 'image' in request.FILES:
+            post.Image = request.FILES['image']
+        post.name = request.POST['name']
+        post.address = request.POST['address']
+        post.description = request.POST['description']
+
+        post.save()
+        return redirect('detail', post.id)
+    else:
+        return render(request, 'update.html', {'designer': post})
 
